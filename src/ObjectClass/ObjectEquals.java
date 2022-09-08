6157ca924291b3303632d0bb940db4e65ddb19ee
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2022-09-08 09:28:06 +0300

    Object and his metods2

--- src/ObjectClass/ObjectEquals.java
+++ src/ObjectClass/ObjectEquals.java
@@ -3,18 +3,21 @@ package ObjectClass;
 
 public class ObjectEquals {
     public static void main(String[] args) {
-        PersonO tom = new PersonO("Tom");
-        PersonO bob = new PersonO("Bob");
+        PersonO tom = new PersonO("Tom",5);
+        PersonO bob = new PersonO("Bob",5);
         System.out.println(tom.equals(bob)); //false
 
-        PersonO tom2 = new PersonO("Tom");//true
+        PersonO tom2 = new PersonO("Tom",3);//true if age=3 false
         System.out.println(tom.equals(tom2));
     }
 }
 class PersonO {
     private String name;
-    public PersonO(String name){
+    private int age;
+    public PersonO(String name,int age){
+
         this.name=name;
+        this.age=age;
     }
     @Override
     public boolean equals (Object obj){
